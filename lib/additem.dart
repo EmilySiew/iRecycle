c0232e90c578f2b2c86156f1af5da6ada9ceaf4e
Emily Siew <emilysiew98@gmail.com>
2021-01-22 03:53:56 +0800

    add item screen

--- lib/additem.dart
+++ lib/additem.dart
@@ -1,17 +1,373 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:recycle/item.dart';
 import 'package:recycle/user.dart';
+import 'package:toast/toast.dart';
 
-class AddItemPage extends StatelessWidget {
+class AddItemPage extends StatefulWidget {
   final User user;
-  const AddItemPage({Key key, @required this.user}) : super(key: key);
+  final Item item;
+  const AddItemPage({Key key, @required this.user, @required this.item}) : super(key: key);
+
+  @override
+  _AddItemPageState createState() => _AddItemPageState();
+}
+
+class _AddItemPageState extends State<AddItemPage> {
+  final TextEditingController _itemnamecontroller = TextEditingController();
+  final TextEditingController _itemweightcontroller = TextEditingController();
+  
+  String _itemname = "";
+  String _itemweight = "";
+  double screenHeight, screenWidth;
+  File _image;
+  String pathAsset = 'assets/images/camera.png';
+  int _radioValue = 0;
+  String category = "Paper";
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "My Accounts",
-        style: TextStyle(fontWeight: FontWeight.w900, fontSize: 28),
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Recycle Item'),
       ),
+      body: Container(
+          child: Padding(
+              padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    GestureDetector(
+                        onTap: () => {_onPictureSelection()},
+                        child: Container(
+                          height: screenHeight / 3.2,
+                          width: screenWidth / 1.8,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: _image == null
+                                  ? AssetImage(pathAsset)
+                                  : FileImage(_image),
+                              fit: BoxFit.cover,
+                            ),
+                            border: Border.all(
+                              width: 3.0,
+                              color: Colors.grey,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(
+                                    5.0) //         <--- border radius here
+                                ),
+                          ),
+                        )),
+                    SizedBox(height: 5),
+                    Text("Click image to take item picture",
+                        style: TextStyle(fontSize: 10.0, color: Colors.black)),
+                    SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Paper"),
+                        new Radio(
+                          value: 0,
+                          groupValue: _radioValue,
+                          onChanged: _handleRadioValueChange,
+                        ),
+                        Text("Metal"),
+                        new Radio(
+                          value: 1,
+                          groupValue: _radioValue,
+                          onChanged: _handleRadioValueChange,
+                        ),
+                        Text("Plastic"),
+                        new Radio(
+                          value: 2,
+                          groupValue: _radioValue,
+                          onChanged: _handleRadioValueChange,
+                        ),
+                        Text("Glass"),
+                        new Radio(
+                          value: 3,
+                          groupValue: _radioValue,
+                          onChanged: _handleRadioValueChange,
+                        ),
+                      ],
+                    ),
+                    TextField(
+                        controller: _itemnamecontroller,
+                        keyboardType: TextInputType.name,
+                        decoration: InputDecoration(
+                            labelText: 'Item Name',
+                            icon: Icon(Icons.title_outlined))),
+                    TextField(
+                        controller: _itemweightcontroller,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            labelText: 'Weight', icon: Icon(Icons.line_weight_outlined))),
+          
+                    SizedBox(height: 10),
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)),
+                      minWidth: 300,
+                      height: 50,
+                      child: Text('Add Recycle Item'),
+                      color: Colors.black,
+                      textColor: Colors.white,
+                      elevation: 15,
+                      onPressed: newItemDialog,
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              ))),
     );
   }
+  void _handleRadioValueChange(int value) {
+    setState(() {
+      _radioValue = value;
+
+      switch (_radioValue) {
+        case 0:
+          category = "Paper";
+          break;
+        case 1:
+          category = "Metal";
+          break;
+        case 0:
+          category = "Plastic";
+          break;
+        case 1:
+          category = "Glass";
+          break;
+      }
+    });
+  }
+
+  _onPictureSelection() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+            //backgroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20.0))),
+            content: new Container(
+              //color: Colors.white,
+              height: screenHeight / 4,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Take picture from:",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      )),
+                  SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Flexible(
+                          child: MaterialButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                        minWidth: 100,
+                        height: 100,
+                        child: Text('Camera',
+                            style: TextStyle(
+                              color: Colors.black,
+                            )),
+                        //color: Color.fromRGBO(101, 255, 218, 50),
+                        color:Colors.blueGrey,
+                        textColor: Colors.black,
+                        elevation: 10,
+                        onPressed: () =>
+                            {Navigator.pop(context), _chooseCamera()},
+                      )),
+                      SizedBox(width: 10),
+                      Flexible(
+                          child: MaterialButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                        minWidth: 100,
+                        height: 100,
+                        child: Text('Gallery',
+                            style: TextStyle(
+                              color: Colors.black,
+                            )),
+                        //color: Color.fromRGBO(101, 255, 218, 50),
+                        color:Colors.blueGrey,
+                        textColor: Colors.black,
+                        elevation: 10,
+                        onPressed: () => {
+                          Navigator.pop(context),
+                          _chooseGallery(),
+                        },
+                      )),
+                    ],
+                  ),
+                ],
+              ),
+            ));
+      },
+    );
+  }
+
+  void _chooseCamera() async {
+    // ignore: deprecated_member_use
+    _image = await ImagePicker.pickImage(
+        source: ImageSource.camera, maxHeight: 800, maxWidth: 800);
+    //_cropImage();
+    setState(() {
+    });
+  }
+  
+  void _chooseGallery() async {
+    // ignore: deprecated_member_use
+    _image = await ImagePicker.pickImage(
+        source: ImageSource.gallery, maxHeight: 400);
+    //_cropImage();
+    setState(() {});
+  }
+
+  Future<Null> _cropImage() async {
+    File croppedFile = await ImageCropper.cropImage(
+        sourcePath: _image.path,
+        aspectRatioPresets: Platform.isAndroid
+            ? [
+                CropAspectRatioPreset.square,
+              ]
+            : [
+                CropAspectRatioPreset.square,
+              ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Resize',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.square,
+            lockAspectRatio: false),
+        iosUiSettings: IOSUiSettings(
+          title: 'Cropper',
+        ));
+    if (croppedFile != null) {
+      _image = croppedFile;
+      setState(() {
+      });
+    }
+  }
+
+  void newItemDialog() {
+    _itemname = _itemnamecontroller.text;
+    _itemweight = _itemweightcontroller.text;
+
+    if (_itemname == "" && _itemweight == "") {
+      Toast.show(
+        "Fill all required fields",
+        context,
+        duration: Toast.LENGTH_LONG,
+        gravity: Toast.TOP,
+      );
+      return;
+    }
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text(
+            "Add new Recycle Item? ",
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          content: new Text(
+            "Are you sure?",
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text(
+                "Yes",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _onAddItem();
+              },
+            ),
+            new FlatButton(
+              child: new Text(
+                "No",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _onAddItem() {
+    //final dateTime = DateTime.now();
+    _itemname = _itemnamecontroller.text;
+    _itemweight = _itemweightcontroller.text;
+    
+    String base64Image = base64Encode(_image.readAsBytesSync());
+
+    http.post("https://techvestigate.com/irecycle/php/additem.php", body: {
+      "email": widget.user.email,
+      "itemname": _itemname,
+      "itemweight": _itemweight,
+      "category": category,
+      "encoded_string": base64Image,
+      //"image": widget.item.itemid,
+      //"itemid": widget.item.itemid,
+    }).then((res) {
+      print(res.body);
+      if (res.body == "success") {
+        Toast.show(
+          "Success",
+          context,
+          duration: Toast.LENGTH_LONG,
+          gravity: Toast.TOP,
+        );
+        Navigator.pop(context);
+      } else {
+        Toast.show(
+          "Failed",
+          context,
+          duration: Toast.LENGTH_LONG,
+          gravity: Toast.TOP,
+        );
+      }
+    }).catchError((err) {
+      print(err);
+    });
+  }
+
+  
+
 }
